ab4eeacdfeb1c3aa5c4cfd325a09f123f6ce0dd9
svetik <ternasvet@gmail.com>
2019-10-18 01:04:26 +0300

    2 lesson - task 4

--- 2_lesson/4/app.js
+++ 2_lesson/4/app.js
@@ -4,4 +4,28 @@
 // В зависимости от состояния навигации менять текст в кнопке.
 //
 // 4.* Элементы навигации и саму навигацию можно создать с помощью createElement, чтобы они были динамическими.
- '../1/app.js';
+
+document.getElementById('button').addEventListener('click', function (e) {
+    e.stopPropagation();
+
+    let nav = document.querySelector('nav');
+    nav.classList.toggle('visible');
+    let button = document.querySelector('#button');
+    if (nav.classList.contains('visible')) {
+        button.innerHTML = 'Скрыть меню';
+    } else {
+        button.innerHTML = 'Показать меню';
+    }
+});
+
+document.querySelector('body').addEventListener('click', function () {
+    let nav = document.querySelector('nav');
+    if (nav.classList.contains('visible')) {
+        button.innerHTML = 'Показать меню';
+        nav.classList.remove('visible');
+    }
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+    e.stopPropagation();
+});
